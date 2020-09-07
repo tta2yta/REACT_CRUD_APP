12a103dcdc9f3dd9f948abeba3fab4f4397becd4
unknown <tta2yta@gmail.com>
2020-09-07 13:51:43 +0300

    adding features to userlist

--- src/Components/UseList.js
+++ src/Components/UseList.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import{Link} from 'react-router-dom'
+import {ListGroup, ListGroupItem, Button} from 'reactstrap';
 
 export const UseList = () => {
     return (
-        <div>
-            <h2>User List</h2>
-        </div>
+        <ListGroup>
+            <ListGroupItem className="d-flex">
+                <strong>User One</strong>
+                <div className='ml-auto'>
+                    <Link to="/edit/1" className="btn btn-warning mr-1">Edit</Link>
+                    <Button color="danger">Delete</Button>
+                </div>
+                
+
+            </ListGroupItem>
+            
+        </ListGroup>
     )
 }
