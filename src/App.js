db4cc3098f7030a9bfd4069823fbabd1481accc5
unknown <tta2yta@gmail.com>
2020-09-07 11:34:54 +0300

    adding features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Home } from './Components/Home';
+import { AddUser } from './Components/AddUser';
+import { EditUsr } from './Components/EditUsr';
 
 function App() {
   return (
     <div className="App">
       <h>React Crud</h>
+      <Home/>
+      <AddUser />
+      <EditUsr />
     </div>
   );
 }
