cddf9a206f2ca5ef5e59816edf2fd87c65ea1f52
unknown <tta2yta@gmail.com>
2020-09-07 15:54:46 +0300

    adding feature to adduser

--- src/Components/AddUser.js
+++ src/Components/AddUser.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-
+import React, {useContext} from 'react'
+import {Link, useHistory} from 'react-router-dom';
+import{GlobalContext} from '../Context/GlobalState'
 import {Form, FormGroup, Input, Button, Label} from 'reactstrap';
 
 export const AddUser = () => {
+    const {addUser}=useContext(GlobalContext)
+    const history= useHistory();
+    const onSubmit=()=>{
+        const newUser={ id:4, name:'kkk'}
+        addUser(newUser);
+        history.push('/');
+    }
     return (
-      <Form>
+      <Form onSubmit={onSubmit}>
           <FormGroup>
               <Label>Name</Label>
               <Input type="text" placeholder="EnterName"></Input>
--- src/Context/AppReducer.js
+++ src/Context/AppReducer.js
@@ -9,7 +9,7 @@ export default (state, action)=> {
                 })}
         case 'ADD_USER':
             return{
-                users:[action.payload, state.users]
+                users:[action.payload, ...state.users]
             }
         default:
             return state;
