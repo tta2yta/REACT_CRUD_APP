fd0c26d25b5ca95c205ab3d390e490a3f5bace87
unknown <tta2yta@gmail.com>
2020-09-07 16:20:20 +0300

    adding feature to edituser

--- src/Components/EditUser.js
+++ src/Components/EditUser.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import {Link, useHistory} from 'react-router-dom';
 import{GlobalContext} from '../Context/GlobalState'
 import {v4 as uuid} from 'uuid';'
@@ -6,13 +6,20 @@ import {Link} from 'react-router-dom';
 
 import {Form, FormGroup, Input, Button, Label} from 'reactstrap';
 
-export const EditUser = () => {
+export const EditUser = (props) => {
     const [selectedUser, setSelectedUser]=useState({
         id:'',
         name:''
     })
+    const currentUserId = props.match.param.id;
     const {users, editUser}=useContext(GlobalContext)
     const history= useHistory();
+    useEffect(()=>{
+        const userId=currentUserId;
+        const selectedUser=users.find(user => user.id === userId)
+        setSelectedUser(selectedUser)
+
+    })
     const onSubmit=()=>{
         const newUser={ id:uuid(), name:name}
         addUser(newUser);
@@ -29,7 +36,7 @@ export const EditUser = () => {
             <Form onSubmit={onSubmit}>
                 <FormGroup>
                     <Label>Name</Label>
-                    <Input type="text" placeholder="EnterName"></Input>
+                    <Input type="text" placeholder="EnterName" onchange={onchange}></Input>
                 </FormGroup>
                 <Button type="submit">Edit</Button>
                 <Link to="/" className="btn btn-danger ml-2">Cancel</Link>
