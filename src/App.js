d0669f46e2a337d49e1dd61e7d0391987483f714
unknown <tta2yta@gmail.com>
2020-09-07 11:58:34 +0300

    adding route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import { Home } from './Components/Home';
 import { AddUser } from './Components/AddUser';
-import { EditUsr } from './Components/EditUsr';
+import { EditUser } from './Components/EditUser';
 
 function App() {
   return (
     <div className="App">
-      <h>React Crud</h>
-      <Home/>
-      <AddUser />
-      <EditUsr />
+      <Router>
+        <h2>Nav Bar</h2>
+          <switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/add" component={AddUser} />
+          <Route path="/edit:id" component={EditUser} />
+        </switch>
+    
+      </Router>
     </div>
   );
 }
--- src/Components/EditUser.js
+++ src/Components/EditUser.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const EditUsr = () => {
+export const EditUser = () => {
     return (
         <div>
             <h2>Edit User</h2>
